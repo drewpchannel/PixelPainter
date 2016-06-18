3be43eaa0c98a2b4c2dfd3897dd1d5ae2e17c32e
dpcc <drewpcchannel@gmail.com>
2016-06-17 16:39:13 -1000

    changes to dom

--- js/main.js
+++ js/main.js
@@ -2,14 +2,12 @@
 window.onload = function () {
   var painterGenerator = createPaintingAbility();
   for(var i = 0; i < painterGenerator.getColorHeight(); i++){
-    var newColorRow = document.createElement('div');
-    newColorRow.className = 'cRows';
-    newColorRow.id = 'rows' + i;
-    document.getElementById('colors').appendChild(newColorRow);
-
+      var newColorRow = document.createElement('div');
+      newColorRow.className = 'cRows';
+      newColorRow.id = 'rows' + i;
+      document.getElementById('colors').appendChild(newColorRow);
       for(var j = 0; j < painterGenerator.getColorWidth(); j++){
         var newColorCell = document.createElement('div');
-
         countDiv++;
         newColorCell.id = countDiv;
         newColorCell.className = 'squares';
@@ -19,25 +17,25 @@ window.onload = function () {
       }
   }
 
-      for(var k = 0; k < painterGenerator.getRowHeight(); k++){
-      var newRow = document.createElement('div');
-      newRow.className = 'gRows';
-      newRow.id = 'gridRows' + k;
-      document.getElementById('grid').appendChild(newRow);
+  for(var k = 0; k < painterGenerator.getRowHeight(); k++){
+  var newRow = document.createElement('div');
+  newRow.className = 'gRows';
+  newRow.id = 'gridRows' + k;
+  document.getElementById('grid').appendChild(newRow);
 
-        for(var l = 0; l < painterGenerator.getRowWidth(); l++){
-          var newCell = document.createElement('div');
+    for(var l = 0; l < painterGenerator.getRowWidth(); l++){
+      var newCell = document.createElement('div');
 
-          countDiv++;
-          newCell.id = countDiv;
-          newCell.className = 'gridSquares';
+      countDiv++;
+      newCell.id = countDiv;
+      newCell.className = 'gridSquares';
 
-          newCell.addEventListener('mousedown', painterGenerator.clickGrid);
-          newCell.addEventListener('mouseover', painterGenerator.doFillOnHover);
-          newCell.addEventListener('mouseup', painterGenerator.mouseUpUpdate);
+      newCell.addEventListener('mousedown', painterGenerator.clickGrid);
+      newCell.addEventListener('mouseover', painterGenerator.doFillOnHover);
+      newCell.addEventListener('mouseup', painterGenerator.mouseUpUpdate);
 
-          document.getElementById('gridRows' + k).appendChild(newCell);
-      }
+      document.getElementById('gridRows' + k).appendChild(newCell);
+    }
   }
   erasers();
 };
---
+++ js/throw.js
@@ -0,0 +1,5 @@
+
+var x = event.screenX;     // Get the horizontal coordinate
+var y = event.screenY;     // Get the vertical coordinate
+var coor = "X coords: " + x + ", Y coords: " + y;
+console.log(coor);
