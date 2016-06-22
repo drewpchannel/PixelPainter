92d807b1bd5a6b0887e3acdfb7de3f6127a81a36
dpcc <drewpcchannel@gmail.com>
2016-06-21 16:09:44 -1000

    test optimization 9

--- js/fireBaseUpdater.js
+++ js/fireBaseUpdater.js
@@ -2,7 +2,6 @@ var firebaseToCheck = new Firebase("https://clicktesterapp.firebaseio.com/");
 
 firebaseToCheck.on("child_changed", function (childSnapshot, dataSnapshot){
       updatePixels(childSnapshot.val());
-      console.log('this is datasnap  ' + childSnapshot.val());
 /*      var _getCurrentChanges = function (x) {
         childSnapshot.val();
       };
@@ -13,8 +12,8 @@ firebaseToCheck.on("child_changed", function (childSnapshot, dataSnapshot){
 
 function updatePixels (currentState) {
   Object.keys(currentState).forEach(function(key){
-    console.log('this is key  ' + key);
+    console.log('this is key  ' + currentState[key]);
     var x = document.getElementById(key);
-    x.style.backgroundColor = currentState[key].value;
+    x.style.backgroundColor = currentState[key];
   });
 }
