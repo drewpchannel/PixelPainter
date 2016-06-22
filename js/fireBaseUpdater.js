6bce0dc87a6ea5a97b487acc86c2b4507da177fb
dpcc <drewpcchannel@gmail.com>
2016-06-21 19:26:21 -1000

    fixed conflicts

--- js/fireBaseUpdater.js
+++ js/fireBaseUpdater.js
@@ -2,29 +2,13 @@ var firebaseToCheck = new Firebase("https://clicktesterapp.firebaseio.com/");
 
 firebaseToCheck.on("value", function (childSnapshot, dataSnapshot){
   updatePixels(childSnapshot.val());
-
-
-/*      var _getCurrentChanges = function (x) {
-        childSnapshot.val();
-      };
-<<<<<<< HEAD
-      return {
-        getCurrentChanges: _getCurrentChanges
-      };*/
-=======
->>>>>>> origin
 });
 
 function updatePixels (currentState) {
   Object.keys(currentState).forEach(function(key){
     var x = document.getElementById(key);
-<<<<<<< HEAD
     if ( currentState[key] !== x.style.backgroundColor ){
       x.style.backgroundColor = currentState[key];
-=======
-    if(currentState[key].color !== x.style.backgroundColor){
-      x.style.backgroundColor = currentState[key].color;
->>>>>>> origin
     }
   });
 }
