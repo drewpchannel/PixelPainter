70d6485add63b4a209b6ec764d6e683224c3d142
dpcc <drewpcchannel@gmail.com>
2016-06-21 17:00:32 -1000

    test optimization 11

--- js/fireBaseUpdater.js
+++ js/fireBaseUpdater.js
@@ -16,8 +16,7 @@ function updatePixels (currentState) {
   Object.keys(currentState).forEach(function(key){
     var x = document.getElementById(key);
     if ( currentState[key] !== x.style.backgroundColor ){
-      console.log('I found a color that is new');
       x.style.backgroundColor = currentState[key];
-    } else {console.log('i found something repeated');}
+    }
   });
 }
