c30195ffa8a96726e763910eeb5d7ebc786c34da
dpcc <drewpcchannel@gmail.com>
2016-06-25 10:08:13 -1000

    config back in

---
+++ js/CONFIG.js
@@ -0,0 +1,6 @@
+var config = {
+    apiKey: "AIzaSyAz9anS7Hy4471yG8VuGvetW5mfPgl0BO0",
+    authDomain: "pixelmaker-be478.firebaseapp.com",
+    databaseURL: "https://pixelmaker-be478.firebaseio.com",
+    storageBucket: "",
+  };
